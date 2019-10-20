3c0d47f28e3de7ff092cbb7125b0685fa7ac930e
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-10-19 21:16:19 -0700

    Updates

---
+++ precache-manifest.748ef5bd19853f818402977869bf4995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6add60c8869257ab8e8187ddcf27a6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "eadbc19bd5f13fc00f8a",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b13994eefd816df9c339",
+    "url": "/robofriends/static/css/main.23669b42.chunk.css"
+  },
+  {
+    "revision": "eadbc19bd5f13fc00f8a",
+    "url": "/robofriends/static/js/2.479009db.chunk.js"
+  },
+  {
+    "revision": "b13994eefd816df9c339",
+    "url": "/robofriends/static/js/main.52380357.chunk.js"
+  },
+  {
+    "revision": "eb54e97a82f44e946317",
+    "url": "/robofriends/static/js/runtime-main.7aa1f784.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
